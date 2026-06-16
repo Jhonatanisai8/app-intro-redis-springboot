59d4f077521e8a20a5826e511f79f67f9136f661
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-06-15 22:10:56 -0500

    feat: init

---
+++ src/main/java/com/microservice/course/appredisspringboot/AppRedisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.course.appredisspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRedisSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRedisSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/course/appredisspringboot/AppRedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.course.appredisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRedisSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
