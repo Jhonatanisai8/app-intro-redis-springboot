29b833cd7488e0a7acd89478a513e9b2a1627517
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-06-15 22:11:47 -0500

    feat: habilitacion de la caché en la aplicación

--- src/main/java/com/microservice/course/appredisspringboot/AppRedisSpringbootApplication.java
+++ src/main/java/com/microservice/course/appredisspringboot/AppRedisSpringbootApplication.java
@@ -2,8 +2,10 @@ package com.microservice.course.appredisspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AppRedisSpringbootApplication {
 
     public static void main(String[] args) {
